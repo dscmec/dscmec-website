a02a712822a831c48710b4065a692be2a1fd4006
Hemanth R <hemanthrt18@gmail.com>
2020-11-22 09:52:34 +0530

    Update Announcements.js

---
+++ src/pages/Announcements/Announcements.js
@@ -0,0 +1,26 @@
+import React from 'react'
+
+import './Announcements.css'
+
+function Announcements() {
+    return (
+        <div className="announcements" id="announcements">
+            <div className="announcements_header">
+                <h1>Announcements</h1>
+            </div>
+            <div className="announcements_body">
+                <h3>Announcement 1</h3>
+                <h3>Announcement 1</h3>
+                <h3>Announcement 1</h3>
+                <h3>Announcement 1</h3>
+                <h3>Announcement 1</h3>
+                <h3>Announcement 1</h3>
+                <h3>Announcement 1</h3>
+                <h3>Announcement 1</h3>
+                
+            </div>
+        </div>
+    )
+}
+
+export default Announcements
