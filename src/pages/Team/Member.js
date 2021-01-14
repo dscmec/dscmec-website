d8224dacbfab6b95312e01b1cca0ba50382e8e84
joeljosh <jjpadippura123@gmail.com>
2021-01-14 23:21:42 +0530

    updated team page responsiveness

--- src/pages/Team/Member.js
+++ src/pages/Team/Member.js
@@ -4,8 +4,9 @@ import { Image } from 'react-bootstrap';
 import linkedinIcon from "../../assets/img/icon-linkedin.png";
 import './Member.css';
 
-function Member(props){
+export const Member = (props) => {
     return(
+        <>
         <div className="member">
             <Image className="circle-icon" src={props.img} alt="member" />
 
@@ -13,7 +14,28 @@ function Member(props){
                 <Image className="ln-icon" src={linkedinIcon} alt="member" /> 
             </a>
         </div>
+        <div classname="details">
+            <h4><strong>{props.name}</strong></h4>
+            <h5>{props.designation}</h5>
+        </div>
+        </>
     )
 }
 
-export default Member;
+export const MemberResponsive = (props) => {
+    return(
+        <>
+        <div className="member-responsive">
+            <Image className="rect-icon" src={props.img} alt="member" />
+        </div>
+
+        <div classname="details-responsive">
+            <h4><strong>{props.name}</strong></h4>
+            <h5>{props.designation}</h5>
+            <a href={props.link} target="_blank" rel="noreferrer">
+                <Image className="ln-icon-resp" src={linkedinIcon} alt="member" /> 
+            </a>
+        </div>
+        </>
+    )
+}
--- src/pages/Team/Team.js
+++ src/pages/Team/Team.js
@@ -1,71 +1,86 @@
 import React from 'react';
+import {Carousel} from 'react-bootstrap';
 
-import Member from "./Member";
+import {Member,MemberResponsive} from "./Member";
+import rectangle from '../../assets/img/rectangle-icon.jpg'
 import './Team.css'
 
-function Team() {
+export default function Team() {
+    return(
+        <>
+        <TeamNormal />
+        <TeamResponsive />
+        </>
+    )
+    
+}
+
+const TeamNormal = () => {
     let circleIcon = "https://upload.wikimedia.org/wikipedia/commons/thumb/4/4c/Circle_Grey_Solid.svg/1200px-Circle_Grey_Solid.svg.png";
     let link = "https://www.linkedin.com/"
+    let Name = "NAME"
+    let designation = "designation"
+
     return (
-        <div>
+        <div className="team-normal">
             <h1 className="team-heading">Meet Our Team</h1>
             <div className="lead-container">
-                <Member img={circleIcon} link={link} />
+                <Member img={circleIcon} link={link} name={Name} designation={designation} />
             </div>
             <div className="web-team-container">
                 <h1>Web Team</h1>
-                <Member img={circleIcon} link={link} />
+                <Member img={circleIcon} link={link} name={Name} designation={designation} />
                 <div className="row">
                     <div className="col-lg-3">
-                        <Member img={circleIcon} link={link} />
+                        <Member img={circleIcon} link={link} name={Name} designation={designation} />
                     </div>
                     <div className="col-lg-3">
-                        <Member img={circleIcon} link={link} />
+                        <Member img={circleIcon} link={link} name={Name} designation={designation} />
                     </div>
                     <div className="col-lg-3">
-                        <Member img={circleIcon} link={link} />
+                        <Member img={circleIcon} link={link} name={Name} designation={designation} />
                     </div>
                     <div className="col-lg-3">
-                        <Member img={circleIcon} link={link} />
+                        <Member img={circleIcon} link={link} name={Name} designation={designation} />
                     </div>
                 </div>
                 <div className="row">
                     <div className="col-lg-3">
-                        <Member img={circleIcon} link={link} />
+                        <Member img={circleIcon} link={link} name={Name} designation={designation} />
                     </div>
                     <div className="col-lg-3">
-                        <Member img={circleIcon} link={link} />
+                        <Member img={circleIcon} link={link} name={Name} designation={designation} />
                     </div>
                     <div className="col-lg-3">
-                        <Member img={circleIcon} link={link} />
+                        <Member img={circleIcon} link={link} name={Name} designation={designation} />
                     </div>
                     <div className="col-lg-3">
-                        <Member img={circleIcon} link={link} />
+                        <Member img={circleIcon} link={link} name={Name} designation={designation} />
                     </div>
                 </div>
             </div>
             <div className="design-team-container">
                 <h1>Design</h1>
                 <div className="dtop">
-                    <Member img={circleIcon} link={link} />
+                    <Member img={circleIcon} link={link} name={Name} designation={designation} />
                 </div>
                 <div className="row dmid">
                     <div className="col-lg-4">
-                        <Member img={circleIcon} link={link} />
+                        <Member img={circleIcon} link={link} name={Name} designation={designation} />
                     </div>
                     <div className="col-lg-4">
-                        <Member img={circleIcon} link={link} />
+                        <Member img={circleIcon} link={link} name={Name} designation={designation} />
                     </div>
                     <div className="col-lg-4">
-                        <Member img={circleIcon} link={link} />
+                        <Member img={circleIcon} link={link} name={Name} designation={designation} />
                     </div>
                 </div>
                 <div className="row dlow">
                     <div className="col-lg-6">
-                        <Member img={circleIcon} link={link} />
+                        <Member img={circleIcon} link={link} name={Name} designation={designation} />
                     </div>
                     <div className="col-lg-6">
-                        <Member img={circleIcon} link={link} />
+                        <Member img={circleIcon} link={link} name={Name} designation={designation} />
                     </div>
                 </div>
             </div>
@@ -74,28 +89,28 @@ function Team() {
                     <div className="col-sm">
                         <h1 className="mheading">ML/AI</h1>
                         <div className="mtop">
-                            <Member img={circleIcon} link={link} />
+                            <Member img={circleIcon} link={link} name={Name} designation={designation} />
                         </div>
                         <div className="row mbottom">
                             <div className="col-sm">
-                                <Member img={circleIcon} link={link} />
+                                <Member img={circleIcon} link={link} name={Name} designation={designation} />
                             </div>
                             <div className="col-sm">
-                                <Member img={circleIcon} link={link} />
+                                <Member img={circleIcon} link={link} name={Name} designation={designation} />
                             </div>
                         </div>
                     </div>
                     <div className="col-sm">
                         <h1 className="pheading">Publicity</h1>
                         <div>
-                            <Member img={circleIcon} link={link} />
+                            <Member img={circleIcon} link={link} name={Name} designation={designation} />
                         </div>
                         <div className="row">
                             <div className="col-sm">
-                                <Member img={circleIcon} link={link} />
+                                <Member img={circleIcon} link={link} name={Name} designation={designation} />
                             </div>
                             <div className="col-sm">
-                                <Member img={circleIcon} link={link} />
+                                <Member img={circleIcon} link={link} name={Name} designation={designation} />
                             </div>
                         </div>
                     </div>
@@ -104,25 +119,25 @@ function Team() {
             <div className="flutter-team-container">
                 <h1 className="fheading">Flutter</h1>
                 <div className="ftop">
-                    <Member img={circleIcon} link={link} />
+                    <Member img={circleIcon} link={link} name={Name} designation={designation} />
                 </div>
                 <div className="row fmid">
                     <div className="col-lg-4">
-                        <Member img={circleIcon} link={link} />
+                        <Member img={circleIcon} link={link} name={Name} designation={designation} />
                     </div>
                     <div className="col-lg-4">
-                        <Member img={circleIcon} link={link} />
+                        <Member img={circleIcon} link={link} name={Name} designation={designation} />
                     </div>
                     <div className="col-lg-4">
-                        <Member img={circleIcon} link={link} />
+                        <Member img={circleIcon} link={link} name={Name} designation={designation} />
                     </div>
                 </div>
                 <div className="row flow">
                     <div className="col-lg-6">
-                        <Member img={circleIcon} link={link} />
+                        <Member img={circleIcon} link={link} name={Name} designation={designation} />
                     </div>
                     <div className="col-lg-6">
-                        <Member img={circleIcon} link={link} />
+                        <Member img={circleIcon} link={link} name={Name} designation={designation} />
                     </div>
                 </div>
             </div>
@@ -131,22 +146,22 @@ function Team() {
                     <div class="col-sm">
                         <h1 className="oheading">Organization</h1>
                         <div className="otop">
-                            <Member img={circleIcon} link={link} />
+                            <Member img={circleIcon} link={link} name={Name} designation={designation} />
                         </div>
                     </div>
                     <div class="col-sm">
                         <h1>Content</h1>
                         <div className="ctop">
-                            <Member img={circleIcon} link={link} />
+                            <Member img={circleIcon} link={link} name={Name} designation={designation} />
                         </div>
                         <div className="cbottom">
-                            <Member img={circleIcon} link={link} />
+                            <Member img={circleIcon} link={link} name={Name} designation={designation} />
                         </div>
                     </div>
                     <div class="col-sm">
                         <h1 className="mheading">Marketing</h1>
                         <div className="matop">
-                            <Member img={circleIcon} link={link} />
+                            <Member img={circleIcon} link={link} name={Name} designation={designation} />
                         </div>
                     </div>
                 </div>
@@ -156,13 +171,13 @@ function Team() {
                     <div class="col-sm">
                         <h1>Social Media</h1>
                         <div className="smtop">
-                            <Member img={circleIcon} link={link} />
+                            <Member img={circleIcon} link={link} name={Name} designation={designation} />
                         </div>
                     </div>
                     <div class="col-sm">
                         <h1>WTM</h1>
                         <div className="wttop">
-                            <Member img={circleIcon} link={link} />
+                            <Member img={circleIcon} link={link} name={Name} designation={designation} />
                         </div>
                     </div>
                 </div>
@@ -171,4 +186,36 @@ function Team() {
     )
 }
 
-export default Team
+const TeamResponsive = () => {
+    let link = "https://www.linkedin.com/"
+    let Name = "NAME"
+    let designation = "designation"
+
+    return (
+        <div className="team-responsive">
+            <h1 className="team-heading-resp">Meet Our Team</h1>
+            <div className="lead-container-resp">
+                <MemberResponsive img={rectangle} link={link} name={Name} designation={designation} />
+            </div>
+            <div className="web-team-container-resp">
+                <h1>Web Team</h1>
+                <Carousel>
+                    <Carousel.Item>
+                        <MemberResponsive img={rectangle} link={link} name={Name} designation={designation} />
+                    </Carousel.Item>
+                    <Carousel.Item>
+                        <MemberResponsive img={rectangle} link={link} name={Name} designation={designation} />
+                    </Carousel.Item>
+                    <Carousel.Item>
+                        <MemberResponsive img={rectangle} link={link} name={Name} designation={designation} />
+                    </Carousel.Item>
+                    <Carousel.Item>
+                        <MemberResponsive img={rectangle} link={link} name={Name} designation={designation} />
+                    </Carousel.Item>
+                </Carousel>   
+            </div>    
+        </div>
+        
+    )
+}
+
