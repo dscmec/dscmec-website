d7d01712fd74ea946338e2023b03db6bf2ebf843
Vishruth S <vs.vishruth.s@gmail.com>
2021-01-16 15:01:08 +0530

    update 2

--- src/pages/Team/Member.js
+++ src/pages/Team/Member.js
@@ -4,17 +4,38 @@ import { Image } from 'react-bootstrap';
 import linkedinIcon from "../../assets/img/icon-linkedin.png";
 import './Member.css';
 
-function Member(props) {
+export const Member = (props) => {
     return (
-        <div className="member">
-            <Image className="circle-icon" src={props.img} alt="member" />
+        <>
+            <div className="member">
+                <Image className="circle-icon" src={props.img} alt="member" />
 
-            <a href={props.link} target="_blank" rel="noreferrer">
-                <Image className="ln-icon" src={linkedinIcon} alt="member" />
-            </a>
-        </div>
+                <a href={props.link} target="_blank" rel="noreferrer">
+                    <Image className="ln-icon" src={linkedinIcon} alt="member" />
+                </a>
+            </div>
+            <div classname="details">
+                <h4><strong>{props.name}</strong></h4>
+                <h5>{props.designation}</h5>
+            </div>
+        </>
     )
 }
 
+export const MemberResponsive = (props) => {
+    return (
+        <>
+            <div className="member-responsive">
+                <Image className="rect-icon" src={props.img} alt="member" />
+            </div>
 
-export default Member;
+            <div classname="details-responsive">
+                <h4><strong>{props.name}</strong></h4>
+                <h5>{props.designation}</h5>
+                <a href={props.link} target="_blank" rel="noreferrer">
+                    <Image className="ln-icon-resp" src={linkedinIcon} alt="member" />
+                </a>
+            </div>
+        </>
+    )
+}
