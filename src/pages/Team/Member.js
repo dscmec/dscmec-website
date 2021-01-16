8653895700bc989fbc265ab114b3b927fa616907
Vishruth S <vs.vishruth.s@gmail.com>
2021-01-16 14:55:29 +0530

    update

--- src/pages/Team/Member.js
+++ src/pages/Team/Member.js
@@ -4,16 +4,17 @@ import { Image } from 'react-bootstrap';
 import linkedinIcon from "../../assets/img/icon-linkedin.png";
 import './Member.css';
 
-function Member(props){
-    return(
+function Member(props) {
+    return (
         <div className="member">
             <Image className="circle-icon" src={props.img} alt="member" />
 
             <a href={props.link} target="_blank" rel="noreferrer">
-                <Image className="ln-icon" src={linkedinIcon} alt="member" /> 
+                <Image className="ln-icon" src={linkedinIcon} alt="member" />
             </a>
         </div>
     )
 }
+
 
 export default Member;
