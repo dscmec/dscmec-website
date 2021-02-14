0bf13ac9832a067818acb9021daa47a7e6f41f8e
Sidharth S <siddhuinchakattu@gmail.com>
2021-02-14 19:56:37 +0530

    Announcements updated

--- src/App.js
+++ src/App.js
@@ -1,26 +1,25 @@
-import React from 'react';
+import React from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
-import Team from './pages/Team/Team';
-import Navbar from './components/Navbar/Navbar';
-import Landing from './pages/Landing/Landing';
-import './App.css';
-import Announcements from './pages/Announcements/Announcements';
-import Focus from './pages/Focus/Focus';
-import OurTeam from './pages/OurTeam/OurTeam';
-import ContactUs from './pages/ContactUs/ContactUs';
+import Team from "./pages/Team/Team";
+import Navbar from "./components/Navbar/Navbar";
+import Landing from "./pages/Landing/Landing";
+import "./App.css";
+import Announcements from "./pages/Announcements/Announcements";
+import Focus from "./pages/Focus/Focus";
+import OurTeam from "./pages/OurTeam/OurTeam";
+import ContactUs from "./pages/ContactUs/ContactUs";
 
 function App() {
   return (
     <Router>
       <div className="App">
+        <Navbar />
         <Switch>
           <Route path="/team">
-            <Navbar />
             <Team />
           </Route>
           <Route path="/">
-            <Navbar />
             <Landing />
             <Announcements />
             <Focus />
--- src/pages/Focus/Focus.js
+++ src/pages/Focus/Focus.js
@@ -1,19 +1,24 @@
-import React from 'react'
+import React from "react";
 
-import './Focus.css'
+import ripplesYellow from "../../assets/svg/ripples-yellow.svg";
+import "./Focus.css";
 
 function Focus() {
-    return (
-        <div className="focus" id="focus">
-            <h1>Our Focus</h1>
-            <br />
-            <p>
-                DSC MEC seeks to encourage learning dynamically and provide a boost
-                to developers stuck in a rut as well as connecting them with a group of like-minded
-                peers to take on larger projects to change things for the better.
-            </p>
-        </div>
-    )
+  return (
+    <section id="focus">
+      <div className="focus">
+        <img src={ripplesYellow} alt="" className="ripples-yellow" />
+        <h1>Our Focus</h1>
+        <br />
+        <p>
+          DSC MEC seeks to encourage learning dynamically and provide a boost to
+          developers stuck in a rut as well as connecting them with a group of
+          like-minded peers to take on larger projects to change things for the
+          better.
+        </p>
+      </div>
+    </section>
+  );
 }
 
-export default Focus
+export default Focus;
