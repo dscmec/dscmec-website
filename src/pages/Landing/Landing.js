a70c4a2df7bb8e7d88cb897d30ae60c2613096c9
Sidharth S <siddhuinchakattu@gmail.com>
2021-02-16 01:56:09 +0530

    Home page updated

--- src/pages/Landing/Landing.js
+++ src/pages/Landing/Landing.js
@@ -1,40 +1,46 @@
-import React from 'react';
-import { Button } from 'react-bootstrap';
-import { NavHashLink as Link } from 'react-router-hash-link';
-import logoFull from '../../assets/svg/logo-full.svg';
-import scrollDown from '../../assets/svg/scroll-down.svg';
-import mascots from '../../assets/svg/mascots.svg';
+import React from "react";
+import { Button } from "react-bootstrap";
+import { NavHashLink as Link } from "react-router-hash-link";
 
-import './Landing.css';
+import logoFull from "../../assets/svg/logo-full.svg";
+import scrollDown from "../../assets/svg/scroll-down.svg";
+import mascots from "../../assets/svg/mascots.svg";
+import mascotsMobile from "../../assets/svg/mascots-mobile.svg";
+import logoMobile from "../../assets/img/logo-mobile.png";
 
+import "./Landing.css";
 
 function Landing() {
-    return (
-        <section className="landing container-fluid" id="home">
-            <div className=" d-none d-lg-block landingRight">
-                <img className="mascots" src={mascots} alt="Mascots" />
-            </div>
-            <div className="row">
-                <div className="col-lg-8 col-md-12 landingLeft">
-                        <div className="landingLeftHeader">
-                            <img src={logoFull} alt="dsc" />
-                        </div>
-                        <div className="landingLeftInfo">
-                            <h4>Helping students bridge the gap between theory and practice</h4>
-                        </div>
-                        <Button className="joinButton" variant="success">
-                                JOIN NOW
-                        </Button>
-                </div>
-            </div>
-            <div className="scroll-down">
-                <p className="d-none d-md-block d-lg-block d-xl">Powered by Google Developers</p>
-                <Link to="/#announcements" smooth={true} spy="true" duration={2000}>
-                    <img src={scrollDown} alt="Scroll Down" />
-                </Link>
-            </div>
-        </section>
-    );
+  return (
+    <section className="landing container-fluid" id="home">
+      <div className=" d-none d-lg-block landingRight">
+        <img className="mascots" src={mascots} alt="Mascots" />
+      </div>
+      <div className="row">
+        <div className="col-lg-8 col-md-12 landingLeft">
+          <div className="landingLeftHeader">
+            <img src={logoFull} alt="dsc" />
+            <img src={logoMobile} className="logo-mobile" alt="" />
+          </div>
+          <div className="landingLeftInfo">
+            <h4>Helping students bridge the gap between theory and practice</h4>
+          </div>
+          <Button className="joinButton" variant="success">
+            JOIN NOW
+          </Button>
+        </div>
+      </div>
+      <div className="scroll-down">
+        <p className="d-none d-md-block d-lg-block d-xl">
+          Powered by Google Developers
+        </p>
+        <Link to="/#announcements" smooth={true} spy="true" duration={2000}>
+          <img src={scrollDown} alt="Scroll Down" />
+        </Link>
+      </div>
+      <img src={mascotsMobile} className="mascots-mobile" alt="Mascots" />
+    </section>
+  );
 }
 
 export default Landing;
