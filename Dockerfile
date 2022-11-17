69e23bd3b2abbe74530ef991fc923fd013946708
jaison080 <jaisondennis080@gmail.com>
2022-11-17 20:13:29 +0530

    Updated

---
+++ Dockerfile
@@ -0,0 +1,12 @@
+FROM node:16-slim
+
+WORKDIR /app
+EXPOSE 80
+
+RUN bash -c "apt update -y && apt install git -y && apt clean && rm -rf /var/lib/apt/lists/* /tmp/* /var/tmp"
+RUN git clone https://github.com/dscmec/dscmec-website.git . && \
+    npm install -g --force nodemon
+
+RUN npm install
+
+CMD nodemon ./index.js & npm start
---
+++ index.js
@@ -0,0 +1,24 @@
+const { spawn } = require("child_process");
+try {
+  setInterval(() => {
+    const child = spawn(`git pull --rebase origin master`, { shell: true });
+    child.stdout.on("data", (data) => {
+      console.log(`stdout: ${data}`);
+    });
+  
+    child.stderr.on("data", (data) => {
+      console.log(`stderr: ${data}`);
+    });
+  
+    child.on("error", (error) => {
+      console.log(`error: ${error.message}`);
+    });
+  
+    child.on("close", (code) => {
+      console.log(`child process exited with code ${code}`);
+    });
+  }, 2000);
+}
+catch(err) {
+  console.log(err);
+}
