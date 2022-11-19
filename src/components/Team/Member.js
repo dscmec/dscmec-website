1e3551e2327cffa13f1705cacb783fe0378ff6a7
jaison080 <jaisondennis080@gmail.com>
2022-11-19 18:53:04 +0530

    Modified SOme Files

--- src/components/Team/Member.js
+++ src/components/Team/Member.js
@@ -8,10 +8,17 @@ export const Member = (props) => {
   return (
     <>
       <div className="member">
-        <a href={props.link} target="_blank" rel="noreferrer">
-          <Image className="ln-icon" src={linkedinIcon} alt="member" />
-        </a>
-        <Image className="circle-icon" src={props.img} alt="member" />
+        {props.link && (
+          <a href={props.link} target="_blank" rel="noreferrer" style={{position:"absolute",top:"-20px"}}>
+            <Image className="ln-icon" src={linkedinIcon} alt="member" />
+          </a>
+        )}
+
+        <Image
+          className="circle-icon"
+          src={`https://drive.google.com/uc?id=${props.img}`}
+          alt="member"
+        />
 
         <div className="details">
           <h4>{props.name}</h4>
